2a1db1c8e691a97fd208507a194833609f216864
Jonny Lezcano <jlezcanoa@ext.falabella.cl>
2023-06-07 20:43:32 -0400

    feat: add frontend with page login and register

---
+++ frontend-talo/src/components/Header.jsx
@@ -0,0 +1,52 @@
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap';
+
+const Header = () => {
+	const userInfo = { name: 'Abe' }
+
+	return (
+		<header>
+			<Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+				<Container>
+					<LinkContainer to='/'>
+						<Navbar.Brand>MERN Auth</Navbar.Brand>
+					</LinkContainer>
+					<Navbar.Toggle aria-controls='basic-navbar-nav' />
+					<Navbar.Collapse id='basic-navbar-nav'>
+						<Nav className='ms-auto'>
+							<LinkContainer to='/login'>
+								<Nav.Link>
+									<FaSignInAlt /> Sign In
+								</Nav.Link>
+							</LinkContainer>
+							<LinkContainer to='/register'>
+								<Nav.Link>
+									<FaSignOutAlt /> Sign Up
+								</Nav.Link>
+							</LinkContainer>
+							{/* {userInfo ? (
+								<>
+									<NavDropdown title={userInfo.name} id='username'>
+										<LinkContainer to='/profile'>
+											<NavDropdown.Item>Profile</NavDropdown.Item>
+										</LinkContainer>
+										<NavDropdown.Item onClick={logoutHandler}>
+											Logout
+										</NavDropdown.Item>
+									</NavDropdown>
+								</>
+							) : (
+								<>
+									
+								</>
+							)} */}
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</header>
+	)
+}
+
+export default Header
